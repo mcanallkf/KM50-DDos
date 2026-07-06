12ae06db667ec202bec8ff6709a451a4ad2bfce3
mcanallkf <kanallduaempat@gmail.com>
2026-07-06 20:30:31 +0700

    Update syahid.py

--- syahid.py
+++ syahid.py
@@ -75,8 +75,8 @@ def send_request(url, req_id, ua_cycle):
             msg = (f"\033[32m[Request {req_id}]\033[94m Success ({response.status_code}) | "
                    f"\033[38;5;214mDelay: {delay:.3f}s | RT: {elapsed:.3f}s | UA: {headers['User-Agent']}")
         elif response.status_code == 403:
-            msg = (f"[Request {req_id}] 🚫 Forbidden (403) | "
-                   f"Delay: {delay:.3f}s | RT: {elapsed:.3f}s | UA: {headers['User-Agent']}")
+            msg = (f"\033[37m[Request {req_id}]\033[38;5;206m Forbidden (403) | "
+                   f"\033[37mDelay: {delay:.3f}s | RT: {elapsed:.3f}s | UA: {headers['User-Agent']}")
         else:
             msg = (f"[Request {req_id}] ⚠️ Failed ({response.status_code}) | "
                    f"Delay: {delay:.3f}s | RT: {elapsed:.3f}s | UA: {headers['User-Agent']}")
